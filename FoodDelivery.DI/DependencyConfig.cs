c760078d82babf29ef8e3ffe409629d6e88ee232
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-05-11 15:17:23 +0300

    Додали та налаштували FoodDelivery.DI

---
+++ FoodDelivery.DI/DependencyConfig.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using AutoMapper;
+using FoodDelivery.BLL.Mapping;
+using FoodDelivery.BLL.Services;
+using FoodDelivery.BLL.Services.Interfaces;
+using FoodDelivery.DAL.Data;
+using FoodDelivery.DAL.Repositories;
+using FoodDelivery.DAL.Repositories.Interfaces;
+using FoodDelivery.DAL.UoW;
+
+namespace FoodDelivery.DI
+{
+    public static class DependencyConfig
+    {
+        public static ContainerBuilder ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Регіструємо контекст
+            builder.RegisterType<AppDbContext>().AsSelf().InstancePerLifetimeScope();
+
+            // Регіструємо репозиторії
+            builder.RegisterType<DishRepository>().As<IDishRepository>();
+            builder.RegisterType<MenuRepository>().As<IMenuRepository>();
+            builder.RegisterType<OrderRepository>().As<IOrderRepository>();
+
+            // Регіструємо UoW
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+            // Регіструємо AutoMapper
+            builder.Register(ctx => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            })).AsSelf().SingleInstance();
+
+            builder.Register(ctx =>
+            {
+                var mapper = ctx.Resolve<MapperConfiguration>().CreateMapper();
+                return mapper;
+            }).As<IMapper>().InstancePerLifetimeScope();
+
+            return builder;
+        }
+    }
+}
