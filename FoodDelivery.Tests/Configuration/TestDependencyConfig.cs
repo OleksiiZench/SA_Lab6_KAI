eaa433f599852737a0afb09c78650fb21c7c2513
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-05-11 16:14:30 +0300

    Додали xUnit Test Project та налаштували

---
+++ FoodDelivery.Tests/Configuration/TestDependencyConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Autofac;
+using AutoMapper;
+using FoodDelivery.BLL.Mapping;
+using FoodDelivery.BLL.Services;
+using FoodDelivery.BLL.Services.Interfaces;
+using FoodDelivery.DAL.UoW;
+using NSubstitute;
+
+
+namespace FoodDelivery.Tests.Configuration
+{
+    public static class TestDependencyConfig
+    {
+        public static ContainerBuilder ConfigureTestContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Створюємо mocks використовуючи NSubstitute
+            var mockUnitOfWork = Substitute.For<IUnitOfWork>();
+
+            // Реєструємо moked UoW
+            builder.RegisterInstance(mockUnitOfWork).As<IUnitOfWork>();
+
+            // Реєструємо AutoMapper
+            builder.Register(ctx => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            })).AsSelf().SingleInstance();
+
+            builder.Register(ctx =>
+            {
+                var mapper = ctx.Resolve<MapperConfiguration>().CreateMapper();
+                return mapper;
+            }).As<IMapper>().InstancePerLifetimeScope();
+
+            // Реєструємо services
+            builder.RegisterType<DishService>().As<IDishService>();
+            builder.RegisterType<MenuService>().As<IMenuService>();
+            builder.RegisterType<OrderService>().As<IOrderService>();
+
+            return builder;
+        }
+    }
+}
---
+++ FoodDelivery.Tests/Fixtures/TestFixture.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using AutoFixture;
+using FoodDelivery.Tests.Configuration;
+
+namespace FoodDelivery.Tests.Fixtures
+{
+    public class TestFixture : IDisposable
+    {
+        public IContainer Container { get; }
+        public Fixture AutoFixture { get; }
+
+        public TestFixture()
+        {
+            var builder = TestDependencyConfig.ConfigureTestContainer();
+            Container = builder.Build();
+            AutoFixture = new Fixture();
+        }
+
+        public void Dispose()
+        {
+            Container.Dispose();
+        }
+    }
+}
